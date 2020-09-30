2475927abad70aa518378bdd79e6b293872a26cc
repl.it user <replituser@example.com>
2020-09-30 10:48:58 +0000

    Added code to display an ASCII robot with dynamic eyes.

---
+++ 1-basics/1-output/escape_character.py
@@ -0,0 +1,9 @@
+# display escape characters
+print("\n display a new line")
+print("\t Display a new tab space")
+print("\\ Display a back slash")
+print("\" Display a double quote")
+print("\' Display a single quote")
+print("")
+print("\t I am Beep!")
+
---
+++ 1-basics/input/user_input.py
@@ -0,0 +1,13 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input()
+print("It is nice to meet you human", name)
+
+print("please enter a character for the eye")
+eye = input()
+
+# Display a box
+print("##########")
+print("# ", eye, "", eye, " #")
+print("#  ----  #")
+print("##########")
