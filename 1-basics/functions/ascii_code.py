42a32574b34bde6a94629bad49a121def89aa156
repl.it user <replituser@example.com>
2020-10-13 09:54:25 +0000

    Added code to use functionord

---
+++ 1-basics/functions/ascii_code.py
@@ -0,0 +1,17 @@
+print("Program started!")
+
+print("please enter a standard character:")
+letter = input()
+
+ord(letter)
+
+
+if (len(letter) == 1):
+  print("The ASCII code for", letter,"is", (ord (letter)))
+
+else:
+  print("enter 1 letter")
+
+print("Program Ended!")
+
+
