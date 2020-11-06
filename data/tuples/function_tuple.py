1c75ecb02aa22bd25ccc82cc757d57355c47f4f4
repl.it user <replituser@example.com>
2020-11-06 16:58:18 +0000

    added code to find the man / min values of a tuple.

---
+++ data/tuples/function_tuple.py
@@ -0,0 +1,4 @@
+def likelihood():
+  likelihoods = (50,38,27,99,4)
+  print("the minimum likelihood of faling is: {}".format(min(likelihoods)))
+  print("the maximum likelihood of not faling is: {}".format(max(likelihoods)))
