22c1e629cb3f3eebc5f6eeb3029ffda9d2b6018d
repl.it user <replituser@example.com>
2020-11-24 11:33:08 +0000

    added code to visualise a shifting sine wave.

--- visual/subplots/csv_subplots.py
+++ visual/subplots/csv_subplots.py
@@ -1,28 +1,35 @@
-import csv
 import matplotlib.pyplot as plt
+import csv
 
 def read_data():
+  with open('visual/subplots/temps.csv') as file:
+    csv_reader = csv.reader(file)
+
+    # extract headers
+    line = next(csv_reader)
+    weeks = [week.strip() for week in line]
+
+    # extract temperature data
+    data = {weeks[0]:[], weeks[1]:[]}
+
+    for line in csv_reader:
+      data[weeks[0]].append(float(line[0].strip()))
+      data[weeks[1]].append(float(line[1].strip()))
   
-  with open("visual/subplots/weeks.csv") as csvfile:
-    csv_reader = csv.reader(csvfile)
-    header = next(csv_reader)
-    temps = {}
-    temps = {
-      header = [0].strip():[]
-      header = [1].strip():[]
-    }
-    for row in csv_reader:
-      temps[header[0]].append(row[0].strip())
-      temps[header[1]].append(row[0].strip())
-    return temps
+  return data
 
 def run():
   data = read_data()
-  fig, axes = plt.subplots(2, 2)
+  weeks = list(data.keys())
+  
+  fig, axs = plt.subplots(2, 1)
+  
+  ax_index = 0
+  for week in weeks:
+    axs[ax_index].plot(range(len(data[week])), data[week])
+    ax_index += 1
 
-  axes[0, 0].plot(data)
-  axes[0, 1].plot(data)
-
+  plt.tight_layout()
   plt.show()
 
 run()
